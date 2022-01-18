df3c8a67e04f6337ebb2d90380055c1ebae1e4d0
Eric R <eric92rodriguez@gmail.com>
2022-01-18 02:08:25 -0300

    fix js

--- script.js
+++ script.js
@@ -1,41 +1,16 @@
 const getValueInput = (value) =>
 {
     let inputValue = document.getElementById("domTextElement").value;
-
     {value === 0? frase = encript(inputValue): frase = desencript(inputValue)}
     document.getElementById("valueInput").innerHTML = frase;
 }
 
 function encript(frase) {
-    let letraE = /e/g;
-    let letraI = /i/g;
-    let letraA = /a/g;
-    let letraO = /o/g;
-    let letraU = /u/g;
-
-    let stre = frase.replace(letraE, "enter");
-    let stri = stre.replace(letraI, "imes");
-    let stra = stri.replace(letraA, "ai");
-    let stro = stra.replace(letraO, "ober");
-    let stru = stro.replace(letraU, "ufat");
-
+    let stru = frase.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
     return stru
-
 }
 
 function desencript(frase) {
-
-    let letraE = /enter/g;
-    let letraI = /imes/g;
-    let letraA = /ai/g;
-    let letraO = /ober/g;
-    let letraU = /ufat/g;
-
-    let stre = frase.replace(letraE, "e");
-    let stri = stre.replace(letraI, "i");
-    let stra = stri.replace(letraA, "a");
-    let stro = stra.replace(letraO, "o");
-    let stru = stro.replace(letraU, "u");
-
+    let stru = frase.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
     return stru
 }
